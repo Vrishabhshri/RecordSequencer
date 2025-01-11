d2b3bf5902f32062e5b0f4b42f695eb9a043c54f
Vrishabhshri <vrishabhshri@gmail.com>
2025-01-11 14:16:06 -0500

    Initial Commit

---
+++ RecordSequencer/RecordSequencerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecordSequencerApp.swift
+//  RecordSequencer
+//
+//  Created by Vrishabh  on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecordSequencerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecordSequencerTests/RecordSequencerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecordSequencerTests.swift
+//  RecordSequencerTests
+//
+//  Created by Vrishabh  on 1/11/25.
+//
+
+import Testing
+@testable import RecordSequencer
+
+struct RecordSequencerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
